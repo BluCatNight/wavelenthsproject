1d1bb5bfc9206aecf84532da064c0831c3b3d03f
BluCat <blucatnight@gmail.com>
2023-05-17 08:05:24 -0700

    sine wave attemp #2

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-function sin(z,v) {
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.stroke();
-    var counter = 0, x=0,y=180;
+function sin(v) {
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+  var intensity = 10;
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  ctx.stroke();
+  var counter = 0, x=0,y=180;
 
-    //100 iterations
-    var increase = 90/180*Math.PI / 9;
-    for(i=0; i<=z; i+=v){
-
+  //100 iterations
+  var increase = 90/180*Math.PI / 9;
+  for(i=0; i<=720; i+=intensity){
     ctx.moveTo(x,y);
     x = i;
     y =  180 - Math.sin(counter) * 120;
@@ -15,18 +16,13 @@ function sin(z,v) {
 
     ctx.lineTo(x,y);
     ctx.stroke();
-}}
-
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-var value = 10;
-
-slider.onchange = function() {
-  value = slider.value;
-  console.log(value)
-  sin(360,value)
+  }
 }
 
-function update() {
-    
+function onload() {
+  console.log("loaded!")
+}
+
+function button() {
+  sin(document.getElementById("myRange").value)
 }
